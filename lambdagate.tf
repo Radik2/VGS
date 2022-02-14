5e989f8417229ee7dcac2ea71f9e5b2fcde45f91
Radik2 <45200201+Radik2@users.noreply.github.com>
2022-02-14 11:42:25 +0200

    Add files via upload

---
+++ lambdagate.tf
@@ -0,0 +1,98 @@
+provider "aws" {
+   region = "us-east-1"
+}
+
+resource "aws_lambda_function" "myLambda" {
+   function_name = "firstFunction"
+
+   
+   s3_bucket = "radik123"
+   s3_key    = "lambda_function.zip"
+
+   
+   handler = "lambda_function.lambda_handler"
+   runtime = "python3.6"
+
+   role = aws_iam_role.lambda_role.arn
+}
+
+
+resource "aws_iam_role" "lambda_role" {
+   name = "role_lambda"
+
+   assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_api_gateway_rest_api" "apiLambda" {
+  name        = "myAPI"
+}
+
+
+
+resource "aws_api_gateway_resource" "api_res" {
+   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+   parent_id   = aws_api_gateway_rest_api.apiLambda.root_resource_id
+   path_part   = "mydemoresource"
+}
+
+resource "aws_api_gateway_method" "Method" {
+   rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
+   resource_id   = aws_api_gateway_resource.api_res.id
+   http_method   = "POST"
+   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+   resource_id = aws_api_gateway_method.Method.resource_id
+   http_method = aws_api_gateway_method.Method.http_method
+
+   integration_http_method = "POST"
+   type                    = "AWS"
+   uri                     = aws_lambda_function.myLambda.invoke_arn
+}
+
+
+
+
+
+resource "aws_api_gateway_deployment" "apideploy" {
+   depends_on = [
+     aws_api_gateway_integration.lambda
+   ]
+
+   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+   stage_name  = "test"
+}
+
+
+resource "aws_lambda_permission" "apigw" {
+   statement_id  = "AllowAPIGatewayInvoke"
+   action        = "lambda:InvokeFunction"
+   function_name = aws_lambda_function.myLambda.function_name
+   principal     = "apigateway.amazonaws.com"
+
+   # The "/*/*" portion grants access from any method on any resource
+   # within the API Gateway REST API.
+   source_arn = "${aws_api_gateway_rest_api.apiLambda.execution_arn}/*/*"
+}
+
+
+output "base_url" {
+  value = aws_api_gateway_deployment.apideploy.invoke_url
+}
